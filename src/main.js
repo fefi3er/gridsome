a6fafdd7a97b0d6ad01c2c3dda547711f8f56597
fefi3er <403125407@qq.com>
2020-12-10 10:05:59 +0800

    'push'

--- src/main.js
+++ src/main.js
@@ -10,10 +10,10 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-import Vant from 'vant'
-import 'vant/lib/vant-css/index.css'
+// import Vant from 'vant'
+// import 'vant/lib/vant-css/index.css'
 
-Vue.use(Vant)
+// Vue.use(Vant)
 Vue.use(ElementUI)
 Vue.use(mavonEditor)
 
